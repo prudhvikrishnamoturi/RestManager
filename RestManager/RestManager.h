b2bf0a7136a77939acd6a529337c295edb8f8964
Prudhvi <prudhvikrishna.moturi@criticalriver.com>
2022-05-18 12:54:14 +0530

    First Commit

---
+++ RestManager/RestManager.h
@@ -0,0 +1,18 @@
+//
+//  RestManager.h
+//  RestManager
+//
+//  Created by Prudhvi on 18/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RestManager.
+FOUNDATION_EXPORT double RestManagerVersionNumber;
+
+//! Project version string for RestManager.
+FOUNDATION_EXPORT const unsigned char RestManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestManager/PublicHeader.h>
+
+
