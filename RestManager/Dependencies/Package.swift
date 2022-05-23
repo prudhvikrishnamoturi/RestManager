4da431f53d1dfaf62adbc4289725c5182eb4843f
Prudhvi <prudhvikrishna.moturi@criticalriver.com>
2022-05-23 17:47:51 +0530

    added package file

--- RestManager/Dependencies/Package.swift
+++ RestManager/Dependencies/Package.swift
@@ -9,13 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Dependencies",
-            targets: ["Dependencies"]),
-        .iOS(.v13),
-        .macOS(.v10_14)
+            targets: ["Dependencies"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/prudhvikrishnamoturi/RestManager", from: "0.1.0"),
+         .package(url: "https://github.com/prudhvikrishnamoturi/RestManager.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
